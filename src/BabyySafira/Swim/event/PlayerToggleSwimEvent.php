902052cb2aee7ce1e7b5049778ca631d5df580ae
Putri Safira <96585861+BabyySafira@users.noreply.github.com>
2022-01-01 22:43:41 +0800

    Add files via upload

--- src/BabyySafira/Swim/event/PlayerToggleSwimEvent.php
+++ src/BabyySafira/Swim/event/PlayerToggleSwimEvent.php
@@ -1,1 +1,23 @@
+<?php
 
+declare(strict_types=1);
+
+namespace BabyySafira\Swim\event;
+
+use pocketmine\event\Cancellable;
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\player\Player;
+
+class PlayerToggleSwimEvent extends PlayerEvent implements Cancellable {
+
+    protected $isSwimming;
+
+    public function __construct(Player $player, bool $isSwimming) {
+        $this->player = $player;
+        $this->isSwimming = $isSwimming;
+    }
+
+    public function isSwimming(): bool {
+        return $this->isSwimming;
+    }
+}
